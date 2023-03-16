32fabaed623f7065d9781e3acf5a94b976ee97a7
Pond (satorn) <107555040+pondsan1412@users.noreply.github.com>
2023-03-17 02:58:34 +0700

    Delete config.py

--- cog/config.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-#discord token pond bot
-discord_token = str("MTA3MjQ2MzI2MTYxOTk4NjU1Mw.GGcPku.Uk43wBBQieJdmpCprdulKRwjcKoH3ewHWk2ifY")
-#str 
-ch_onready = int(1039904905328074825)#this is text channel id "general"
-#mario kart tracks photos parameter 
-babypark = "https://raw.githubusercontent.com/sheat-git/mk8dx/main/tracks/20230205/20.jpg"
-
