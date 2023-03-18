581c24ed7ba644e3b89fd3908ac6a0308ffa5a57
Zquka <60568693+Zquka@users.noreply.github.com>
2023-03-18 05:45:33 -0400

    Zquka update

--- cog/commands/oldtrack/CrossingCup.py
+++ cog/commands/oldtrack/CrossingCup.py
@@ -9,32 +9,32 @@ class CrossingCup(commands.Cog):
         self.bot = bot
     @commands.hybrid_command(
         name="babypark",
-        description="",
+        description="Baby Park",
         help="Baby Park Track",
         aliases=["dBP","dbp","DBP","dBp","dbP","6-1"]
     )
     async def _track_dBP(self,ctx):
         embedVar = discord.Embed(
-            title="Baby Park",
+            title="Crossing Cup",
             description="GCN Baby Park",
             color=discord.Color.dark_purple()
         )
         embedVar.set_image(url=config.BabyPark)
         embedVar.set_thumbnail(url=config.Crossing)
-        embedVar.add_field(name="Track",value="6-1",inline=False)
+        embedVar.add_field(name="Track Code",value="6-1",inline=False)
         embedVar.set_footer(text=f"Using a command by {ctx.author}")
         await ctx.send(embed=embedVar)
 
 
     @commands.hybrid_command(
         name="cheeseland",
-        description="",
+        description="Cheese Land",
         help="Cheese Land Track",
         aliases=["dCL","dcl","DCl","dcL","DcL", "6-2"]
     )
     async def _track_dCL(self,ctx):
         embedVar = discord.Embed(
-            title="Cheese Land",
+            title="Crossing Cup",
             description="GBA Cheese Land",
             color=discord.Color.dark_purple()
         )
@@ -44,5 +44,44 @@ class CrossingCup(commands.Cog):
         embedVar.set_footer(text=f"Using a command by {ctx.author}")
         await ctx.send(embed=embedVar)
     
+    
+    @commands.hybrid_command(
+        name="wildwoods",
+        description="Wild Woods",
+        help="Wild Woods Track",
+        aliases=["ww","WW","dWW","dww","DWW","DwW","dWw","dWw","dwW","6-3"]
+    )
+    async def _track_dBP(self,ctx):
+        embedVar = discord.Embed(
+            title="Crossing Cup",
+            description="Wild Woods",
+            color=discord.Color.dark_purple()
+        )
+        embedVar.set_image(url=config.WildWoods)
+        embedVar.set_thumbnail(url=config.Crossing)
+        embedVar.add_field(name="Track Code",value="6-3",inline=False)
+        embedVar.set_footer(text=f"Using a command by {ctx.author}")
+        await ctx.send(embed=embedVar)
+
+
+    @commands.hybrid_command(
+        name="animalcrossing",
+        description="Animal Crossing",
+        help="Animal Crossing Track",
+        aliases=["ac","AC","dAC","dac","DAC","DaC","dAc","dAc","daC","6-4"]
+    )
+    async def _track_dBP(self,ctx):
+        embedVar = discord.Embed(
+            title="Crossing Cup",
+            description="Animal Crossing",
+            color=discord.Color.dark_purple()
+        )
+        embedVar.set_image(url=config.WildWoods)
+        embedVar.set_thumbnail(url=config.Crossing)
+        embedVar.add_field(name="Track",value="6-4",inline=False)
+        embedVar.set_footer(text=f"Using a command by {ctx.author}")
+        await ctx.send(embed=embedVar)
+
+
 async def setup(bot):
     await bot.add_cog(CrossingCup(bot))
