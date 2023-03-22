eef518a9de6039c80c9ae840dfef92e3793530cc
Pond (satorn) <107555040+pondsan1412@users.noreply.github.com>
2023-03-23 05:17:54 +0700

    update

--- Runbot.py
+++ Runbot.py
@@ -4,7 +4,7 @@ import os
 from cog import config
 from cog import secret
 from googletrans import Translator
-from event import eventbot
+from cog.commands.event import eventbot
 class bot(commands.Bot):
     def __init__(self):
         super().__init__(
@@ -19,31 +19,36 @@ class bot(commands.Bot):
         ch_ready = int(secret.ch_onready)
         print(ch_ready)
     async def setup_hook(self):
+        extensions = [
+    "cog.commands.General",
+    "cog.commands.event",
+    "cog.commands.error",
+    "cog.commands.oldtrack.1MushroomCup",
+    "cog.commands.oldtrack.2FlowerCup",
+    "cog.commands.oldtrack.3StarCup",
+    "cog.commands.oldtrack.4SpecialCup",
+    "cog.commands.oldtrack.5EggCup",
+    "cog.commands.oldtrack.6CrossingCup",
+    "cog.commands.oldtrack.7ShellCup",
+    "cog.commands.oldtrack.8BananaCup",
+    "cog.commands.oldtrack.9LeafCup",
+    "cog.commands.oldtrack.10LightningCup",
+    "cog.commands.oldtrack.11TriforceCup",
+    "cog.commands.oldtrack.12BellCup",
+    "cog.commands.dlctrack.13GoldenDashCup",
+    "cog.commands.dlctrack.14LuckyCatCup",
+    "cog.commands.dlctrack.15TurnipCup",
+    "cog.commands.dlctrack.16PropellerCup",
+    "cog.commands.dlctrack.17RockCup",
+    "cog.commands.dlctrack.18MoonCup",
+    "cog.commands.dlctrack.19FruitCup",
+    "cog.commands.dlctrack.20BoomerangCup"
+]
         bot.remove_command("help")
-        await bot.load_extension("cog.commands.General")
-        await bot.load_extension("event")
-        await bot.load_extension("cog.commands.error")
-        await bot.load_extension("cog.commands.oldtrack.1MushroomCup")
-        await bot.load_extension("cog.commands.oldtrack.2FlowerCup")
-        await bot.load_extension("cog.commands.oldtrack.3StarCup")
-        await bot.load_extension("cog.commands.oldtrack.4SpecialCup")
-        await bot.load_extension("cog.commands.oldtrack.5EggCup")        
-        await bot.load_extension("cog.commands.oldtrack.6CrossingCup")
-        await bot.load_extension("cog.commands.oldtrack.7ShellCup")
-        await bot.load_extension("cog.commands.oldtrack.8BananaCup")
-        await bot.load_extension("cog.commands.oldtrack.9LeafCup")
-        await bot.load_extension("cog.commands.oldtrack.10LightningCup")
-        await bot.load_extension("cog.commands.oldtrack.11TriforceCup")
-        await bot.load_extension("cog.commands.oldtrack.12BellCup")
-        await bot.load_extension("cog.commands.dlctrack.13GoldenDashCup")
-        await bot.load_extension("cog.commands.dlctrack.14LuckyCatCup")
-        await bot.load_extension("cog.commands.dlctrack.15TurnipCup")
-        await bot.load_extension("cog.commands.dlctrack.16PropellerCup")
-        await bot.load_extension("cog.commands.dlctrack.17RockCup")
-        await bot.load_extension("cog.commands.dlctrack.18MoonCup")
-        await bot.load_extension("cog.commands.dlctrack.19FruitCup")
-        await bot.load_extension("cog.commands.dlctrack.20BoomerangCup")
-
+        for extension in extensions:
+            await bot.load_extension(extension)
+ 
+              
         await bot.tree.sync()
     
        
