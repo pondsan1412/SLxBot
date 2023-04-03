eaac44344edf2c3e272ed80193a91e565273d6b8
Pond (satorn) <107555040+pondsan1412@users.noreply.github.com>
2023-04-03 23:46:36 +0700

    Update SelectMenus.py

--- cog/SelectMenus.py
+++ cog/SelectMenus.py
@@ -11,102 +11,102 @@ class Select(discord.ui.Select):
             description="Show all commands"
             ),
             discord.SelectOption(
-            label="Mushroom Cup",
+            label="Mushroom Cup Commands",
             emoji=config.MushroomCup,
             description="Mushroom Cup Track List"
             ),
             discord.SelectOption(
-            label="Flower Cup",
+            label="Flower Cup Commands",
             emoji=config.FlowerCup,
             description="Flower Cup Track List"
             ),
             discord.SelectOption(
-            label="Star Cup",
+            label="Star Cup Commands",
             emoji=config.StarCup,
             description="Star Cup Track List"
             ),
             discord.SelectOption(
-            label="Special Cup",
+            label="Special Cup Commands",
             emoji=config.SpecialCup,
             description="Special Cup Track List"
             ),
             discord.SelectOption(
-            label="Egg Cup",
+            label="Egg Cup Commands",
             emoji=config.EggCup,
             description="Egg Cup Track List"
             ),
             discord.SelectOption(
-            label="Crossing Cup",
+            label="Crossing Cup Commands",
             emoji=config.CrossingCup,
             description="Crossing Cup Track List"
             ),
             discord.SelectOption(
-            label="Shell Cup",
+            label="Shell Cup Commands",
             emoji=config.ShellCup,
             description="Shell Cup Track List"
             ),
             discord.SelectOption(
-            label="Banana Cup",
+            label="Banana Cup Commands",
             emoji=config.BananaCup,
             description="Banana Cup Track List"
             ),
             discord.SelectOption(
-            label="Leaf Cup",
+            label="Leaf Cup Commands",
             emoji=config.LeafCup,
             description="Leaf Cup Track List"
             ),
             discord.SelectOption(
-            label="Lightning Cup",
+            label="Lightning Cup Commands",
             emoji=config.LightningCup,
             description="Lightning Cup Track List"
             ),
             discord.SelectOption(
-            label="Triforce Cup",
+            label="Triforce Cup Commands",
             emoji=config.TriforceCup,
             description="Triforce Cup Track List"
             ),
             discord.SelectOption(
-            label="Bell Cup",
+            label="Bell Cup Commands",
             emoji=config.BellCup,
             description="Bell Cup Track List"
             ),
             discord.SelectOption(
-            label="Golden Dash Cup",
+            label="Golden Dash Cup Commands",
             emoji=config.GoldenDashCup,
             description="Golden Dash Cup Track List"
             ),
             discord.SelectOption(
-            label="Lucky Cat Cup",
+            label="Lucky Cat Cup Commands",
             emoji=config.LuckyCatCup,
             description="Lucky Cat Cup Track List"
             ),
             discord.SelectOption(
-            label="Turnip Cup",
+            label="Turnip Cup Commands",
             emoji=config.TurnipCup,
             description="Turnip Cup Track List"
             ),
             discord.SelectOption(
-            label="Propeller Cup",
+            label="Propeller Cup Commands",
             emoji=config.PropellerCup,
             description="Propeller Cup Track List"
             ),
             discord.SelectOption(
-            label="Rock Cup",
+            label="Rock Cup Commands",
             emoji=config.RockCup,
             description="Rock Cup Track List"
             ),
             discord.SelectOption(
-            label="Moon Cup",
+            label="Moon Cup Commands",
             emoji=config.MoonCup,
             description="Moon Cup Track List"
             ),
             discord.SelectOption(
-            label="Fruit Cup",
+            label="Fruit Cup Commands",
             emoji=config.FruitCup,
             description="Fruit Cup Track List"
             ),
             discord.SelectOption(
-            label="Boomerang Cup",
+            label="Boomerang Cup Commands",
             emoji=config.BoomerangCup,
             description="Boomerang Cup Track List"
             ),
