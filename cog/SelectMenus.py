f1ca345b1efad95da9bb6da12e1257962980ab3e
Zquka <60568693+Zquka@users.noreply.github.com>
2023-06-29 07:47:25 -0400

    Rename

--- cog/SelectMenus.py
+++ cog/SelectMenus.py
@@ -11,102 +11,102 @@ class Select(discord.ui.Select):
             description="Show all commands"
             ),
             discord.SelectOption(
-            label="Mushroom Cup Commands",
+            label="Mushroom Cup",
             emoji=config.MushroomCup,
             description="Mushroom Cup Track List"
             ),
             discord.SelectOption(
-            label="Flower Cup Commands",
+            label="Flower Cup",
             emoji=config.FlowerCup,
             description="Flower Cup Track List"
             ),
             discord.SelectOption(
-            label="Star Cup Commands",
+            label="Star Cup",
             emoji=config.StarCup,
             description="Star Cup Track List"
             ),
             discord.SelectOption(
-            label="Special Cup Commands",
+            label="Special Cup",
             emoji=config.SpecialCup,
             description="Special Cup Track List"
             ),
             discord.SelectOption(
-            label="Egg Cup Commands",
+            label="Egg Cup",
             emoji=config.EggCup,
             description="Egg Cup Track List"
             ),
             discord.SelectOption(
-            label="Crossing Cup Commands",
+            label="Crossing Cup",
             emoji=config.CrossingCup,
             description="Crossing Cup Track List"
             ),
             discord.SelectOption(
-            label="Shell Cup Commands",
+            label="Shell Cup",
             emoji=config.ShellCup,
             description="Shell Cup Track List"
             ),
             discord.SelectOption(
-            label="Banana Cup Commands",
+            label="Banana Cup",
             emoji=config.BananaCup,
             description="Banana Cup Track List"
             ),
             discord.SelectOption(
-            label="Leaf Cup Commands",
+            label="Leaf Cup",
             emoji=config.LeafCup,
             description="Leaf Cup Track List"
             ),
             discord.SelectOption(
-            label="Lightning Cup Commands",
+            label="Lightning Cup",
             emoji=config.LightningCup,
             description="Lightning Cup Track List"
             ),
             discord.SelectOption(
-            label="Triforce Cup Commands",
+            label="Triforce Cup",
             emoji=config.TriforceCup,
             description="Triforce Cup Track List"
             ),
             discord.SelectOption(
-            label="Bell Cup Commands",
+            label="Bell Cup",
             emoji=config.BellCup,
             description="Bell Cup Track List"
             ),
             discord.SelectOption(
-            label="Golden Dash Cup Commands",
+            label="Golden Dash Cup",
             emoji=config.GoldenDashCup,
             description="Golden Dash Cup Track List"
             ),
             discord.SelectOption(
-            label="Lucky Cat Cup Commands",
+            label="Lucky Cat Cup",
             emoji=config.LuckyCatCup,
             description="Lucky Cat Cup Track List"
             ),
             discord.SelectOption(
-            label="Turnip Cup Commands",
+            label="Turnip Cup",
             emoji=config.TurnipCup,
             description="Turnip Cup Track List"
             ),
             discord.SelectOption(
-            label="Propeller Cup Commands",
+            label="Propeller Cup",
             emoji=config.PropellerCup,
             description="Propeller Cup Track List"
             ),
             discord.SelectOption(
-            label="Rock Cup Commands",
+            label="Rock Cup",
             emoji=config.RockCup,
             description="Rock Cup Track List"
             ),
             discord.SelectOption(
-            label="Moon Cup Commands",
+            label="Moon Cup",
             emoji=config.MoonCup,
             description="Moon Cup Track List"
             ),
             discord.SelectOption(
-            label="Fruit Cup Commands",
+            label="Fruit Cup",
             emoji=config.FruitCup,
             description="Fruit Cup Track List"
             ),
             discord.SelectOption(
-            label="Boomerang Cup Commands",
+            label="Boomerang Cup",
             emoji=config.BoomerangCup,
             description="Boomerang Cup Track List"
             ),
@@ -117,53 +117,53 @@ class Select(discord.ui.Select):
             ),
         ]
         super().__init__(
-            placeholder=f"🤏Select some option",
+            placeholder=f"🤏Select some option(s)",
             max_values=1,
             min_values=1,
             options=options
         )
     async def callback(self, int:discord.Interaction):
-        if self.values[0] == "General Command list":
+        if self.values[0] == "General Command List":
             await int.response.edit_message(embed=config.embedGeneral1)
-        elif self.values[0] == "Mushroom Cup Commands":
+        elif self.values[0] == "Mushroom Cup":
             await int.response.edit_message(embed=config.embedMushroomcup)
-        elif self.values[0] == "Flower Cup Commands":
+        elif self.values[0] == "Flower Cup":
             await int.response.edit_message(embed=config.embedFlowercup)
-        elif self.values[0] == "Star Cup Commands":
+        elif self.values[0] == "Star Cup":
             await int.response.edit_message(embed=config.embedStarcup)
-        elif self.values[0] == "Special Cup Commands":
+        elif self.values[0] == "Special Cup":
             await int.response.edit_message(embed=config.embedSpecialcup)
-        elif self.values[0] == "Egg Cup Commands":
+        elif self.values[0] == "Egg Cup":
             await int.response.edit_message(embed=config.embedEggcup)
-        elif self.values[0] == "Crossing Cup Commands":
+        elif self.values[0] == "Crossing Cup":
             await int.response.edit_message(embed=config.embedCrossingcup)
-        elif self.values[0] == "Shell Cup Commands":
+        elif self.values[0] == "Shell Cup":
             await int.response.edit_message(embed=config.embedShellcup)
-        elif self.values[0] == "Banana Cup Commands":
+        elif self.values[0] == "Banana Cup":
             await int.response.edit_message(embed=config.embedBananacup)
-        elif self.values[0] == "Leaf Cup Commands":
+        elif self.values[0] == "Leaf Cup":
             await int.response.edit_message(embed=config.embedLeafcup)
-        elif self.values[0] == "Lightning Cup Commands":
+        elif self.values[0] == "Lightning Cup":
             await int.response.edit_message(embed=config.embedLightningcup)
-        elif self.values[0] == "Triforce Cup Commands":
+        elif self.values[0] == "Triforce Cup":
             await int.response.edit_message(embed=config.embedTriforcecup)
-        elif self.values[0] == "Bell Cup Commands":
+        elif self.values[0] == "Bell Cup":
             await int.response.edit_message(embed=config.embedBellcup)
-        elif self.values[0] == "Golden Dash Cup Commands":
+        elif self.values[0] == "Golden Dash Cup":
             await int.response.edit_message(embed=config.embedGoldenDashcup)
-        elif self.values[0] == "Lucky Cat Cup Commands":
+        elif self.values[0] == "Lucky Cat Cup":
             await int.response.edit_message(embed=config.embedLuckyCatcup)
-        elif self.values[0] == "Turnip Cup Commands":
+        elif self.values[0] == "Turnip Cup":
             await int.response.edit_message(embed=config.embedTurnipcup)
-        elif self.values[0] == "Propeller Cup Commands":
+        elif self.values[0] == "Propeller Cup":
             await int.response.edit_message(embed=config.embedPropellercup)
-        elif self.values[0] == "Rock Cup Commands":
+        elif self.values[0] == "Rock Cup":
             await int.response.edit_message(embed=config.embedRockcup)
-        elif self.values[0] == "Moon Cup Commands":
+        elif self.values[0] == "Moon Cup":
             await int.response.edit_message(embed=config.embedMooncup)
-        elif self.values[0] == "Fruit Cup Commands":
+        elif self.values[0] == "Fruit Cup":
             await int.response.edit_message(embed=config.embedFruitcup)
-        elif self.values[0] == "Boomerang Cup Commands":
+        elif self.values[0] == "Boomerang Cup":
             await int.response.edit_message(embed=config.embedBoomerangcup)
         elif self.values[0] == "Developer Team":
             await int.response.edit_message(embed=config.embedDevsTeam)            
