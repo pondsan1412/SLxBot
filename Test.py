d18aae422dff7ddf337f57537e2ba4c7da9dc154
Pond (satorn) <107555040+pondsan1412@users.noreply.github.com>
2023-09-26 18:26:22 +0700

    d

--- Test.py
+++ Test.py
@@ -1,21 +1,24 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import discord
 from discord.ext import commands
-from cog import secret
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
-client = gspread.authorize(creds)
-sheet = client.open("Silent Lightning TT Leaderboard")
-file_in_sheet_testsheet = sheet.worksheet("Submissions")
+from cog import config
+from cog import embedconfig
+from cog import embedtrack
 
-bot = commands.Bot(command_prefix="?",intents=discord.Intents.all())
-@bot.event
-async def on_ready():
-    print(bot.user)
-@bot.command(name="test")
-async def test(ctx,a,b,c,d):
-    update_row_submit = [a,b,c,d]
-    file_in_sheet_testsheet.insert_row(update_row_submit, 1937)
-    await ctx.send(update_row_submit)
-bot.run(secret.discord_token)
+class trackbot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.Cog.listener()    
+    async def on_message(
+        self,
+        message
+    ):
+        try:
+            if message.author == self.bot.user:
+                return
+            command = message.content.split()[0]
+            if command == "1-1":
+                do what ever here
+        except IndexError:
+            pass
+
+        
